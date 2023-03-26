7ad61ceec3445ae2ec88cbaa0911c6b63615b409
jok <jok@kruso.dk>
2023-03-26 21:48:03 +0200

    Adds fastify

---
+++ src/server/feature/example/example.ts
@@ -0,0 +1,42 @@
+import { FastifyInstance, FastifyRequest } from "fastify";
+
+interface QueryString {
+	name: string;
+}
+
+interface Params {
+	name: string;
+}
+
+interface CustomRouteGenericParam {
+	Params: Params;
+}
+
+interface CustomRouteGenericQuery {
+	Querystring: QueryString;
+}
+
+export const example = async (instance: FastifyInstance) => {
+	instance.get("/", async (req, res) => {
+		res.status(200).send({
+			hello: "World",
+		});
+	});
+
+	instance.register(
+		async (instance: FastifyInstance) => {
+			instance.get("/", async (req: FastifyRequest<CustomRouteGenericQuery>, res) => {
+				const { name = "" } = req.query;
+				res.status(200).send(`Hello ${name}`);
+			});
+
+			instance.get("/:name", async (req: FastifyRequest<CustomRouteGenericParam>, res) => {
+				const { name = "" } = req.params;
+				res.status(200).send({ hello: name });
+			});
+		},
+		{
+			prefix: "/hello",
+		}
+	);
+};
