be43f01f6c47e67be5b032ca7d038e44ae6662e5
jok <jok@kruso.dk>
2023-03-28 21:59:23 +0200

    Add swagger schema and simplify

--- src/server/feature/example/route.ts
+++ src/server/feature/example/route.ts
@@ -1,40 +1,19 @@
 import { FastifyInstance, RequestGenericInterface } from "fastify";
 import { HelloQuerySchema, HelloQueryType, HelloReplySchema, HelloReplyType } from "@server/feature/example/schema";
 
-type GetHello = RequestGenericInterface & {
-	Reply: HelloReplyType;
-};
-
-type GetHelloQuerystring = RequestGenericInterface & {
-	Querystring: HelloQueryType;
-};
-
 type GetHelloParams = RequestGenericInterface & {
 	Params: HelloQueryType;
 	Reply: HelloReplyType;
 };
 
 export const exampleRoute = async (server: FastifyInstance) => {
-	server.get<GetHello>("/", { schema: { response: { 200: HelloReplySchema } } }, async (req, res) => {
-		res.status(200).send({
-			hello: "World",
-		});
-	});
-
 	server.register(
 		async (server: FastifyInstance) => {
-			server.get<GetHelloQuerystring>(
-				"/",
-				{ schema: { querystring: HelloQuerySchema, response: { 200: HelloReplySchema } } },
-				async (req, res) => {
-					const { name = "" } = req.query;
-					res.status(200).send(`Hello ${name}`);
-				}
-			);
+			server.addSchema({ $id: "HelloReplySchema", ...HelloReplySchema });
 
 			server.get<GetHelloParams>(
 				"/:name",
-				{ schema: { params: HelloQuerySchema, response: { 200: HelloReplySchema } } },
+				{ schema: { params: HelloQuerySchema, response: { 200: { $ref: "HelloReplySchema#" } } } },
 				async (req, res) => {
 					const { name = "" } = req.params;
 					res.status(200).send({ hello: name });
