94e9f65c22e0f1efd4e4dc6d960a3de87d97f700
jok <jok@kruso.dk>
2023-03-31 13:34:26 +0200

    Remove server

--- src/server/root.ts
+++
@@ -1,6 +0,0 @@
-import { FastifyInstance } from "fastify";
-import { exampleRoute } from "@server/feature/example/route";
-
-export default async function (instance: FastifyInstance) {
-	instance.register(exampleRoute);
-}
