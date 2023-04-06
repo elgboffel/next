629cab5b4683cc4ff72c137366d9adaca9566baf
jok <jok@kruso.dk>
2023-04-06 19:52:57 +0200

    Fix handle next page catch error

--- src/infrastructure/errors/handle-next-page-catch-error.ts
+++ src/infrastructure/errors/handle-next-page-catch-error.ts
@@ -2,8 +2,8 @@ import { AppError } from "@infrastructure/errors/app-error";
 
 export const handleNextPageCatchError = (e: unknown) => {
 	if (e instanceof AppError) {
-		console.warn(`Api Error while requesting data: ${JSON.stringify(e.serialize())}`);
-		return e.serialize();
+		console.warn(`Api Error while requesting data: ${JSON.stringify(e)}`);
+		return e;
 	}
 
 	throw new Error(`Unknown Api Error while requesting data: ${JSON.stringify(e)}`);
