ce18303d2c1f8d4b3e5bd06c6504febb5b38e017
jok <jok@kruso.dk>
2023-04-12 21:29:33 +0200

    Adds paths

--- src/server/feature/pages/route.ts
+++ src/server/feature/pages/route.ts
@@ -1,15 +1,22 @@
 import { FastifyInstance } from "fastify";
 import { getPages } from "@server/feature/pages/service";
-import { GetPagesQuerySchema, GetPagesReplySchema } from "@server/feature/pages/schema";
+import { GetPagesPathsReplySchema, GetPagesQuerySchema, GetPagesReplySchema } from "@server/feature/pages/schema";
 
 export async function pagesRoute(server: FastifyInstance) {
 	server.addSchema({ $id: "GetPagesResponseSchema", ...GetPagesReplySchema });
+	server.addSchema({ $id: "GetPagesPathsResponseSchema", ...GetPagesPathsReplySchema });
 
 	server.register(
 		async (server: FastifyInstance) => {
 			server.get(
 				"/:areaId",
 				{ schema: { params: GetPagesQuerySchema, response: { 200: { $ref: "GetPagesResponseSchema#" } } } },
+				getPages
+			);
+
+			server.get(
+				"/paths/:areaId",
+				{ schema: { params: GetPagesQuerySchema, response: { 200: { $ref: "GetPagesPathsResponseSchema#" } } } },
 				getPages
 			);
 		},
--- src/server/feature/pages/schema.ts
+++ src/server/feature/pages/schema.ts
@@ -8,8 +8,47 @@ export const GetPagesReplySchema = Type.Array(
 	Type.Object({
 		id: Type.Number(),
 		name: Type.String(),
+		languages: Type.Array(
+			Type.Object({
+				culture: Type.Optional(Type.String()),
+				pageIsPublished: Type.Optional(Type.Boolean()),
+				page: Type.Optional(
+					Type.Object({
+						id: Type.Number(),
+						name: Type.String(),
+					})
+				),
+			})
+		),
+		path: Type.Array(
+			Type.Object({
+				id: Type.Number(),
+				name: Type.String(),
+			})
+		),
 	})
 );
 
 export type GetPagesQueryType = Static<typeof GetPagesQuerySchema>;
 export type GetPagesReplyType = Static<typeof GetPagesReplySchema>;
+
+export const GetPagesPathsReplySchema = Type.Array(
+	Type.Object({
+		id: Type.Number(),
+		name: Type.String(),
+		culture: Type.String(),
+		pageIsPublished: Type.Boolean(),
+		page: Type.Object({
+			id: Type.Number(),
+			name: Type.String(),
+		}),
+		path: Type.Array(
+			Type.Object({
+				id: Type.Number(),
+				name: Type.String(),
+			})
+		),
+	})
+);
+
+export type GetPagesPathsReplyType = Static<typeof GetPagesPathsReplySchema>;
