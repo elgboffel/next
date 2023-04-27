a539452c13a3ba654fed110a2f77d7b7a9de2b13
jok <jok@kruso.dk>
2023-04-27 21:14:13 +0200

    Adds cors, public and private routes

---
+++ src/server/feature/example-private/hello-with-name/handler.ts
@@ -0,0 +1,11 @@
+﻿import { GenericRouteHandler } from "@server/types";
+import { AppError } from "@infrastructure/errors/app-error/app-error";
+import { RouteHandler } from "fastify";
+import { HelloWithNameQueryType, HelloWithNameReplyType } from "@server/feature/example-private/hello-with-name/schema";
+
+export type HelloWithNameHandler = GenericRouteHandler<HelloWithNameQueryType, HelloWithNameReplyType | AppError>;
+
+export const helloWithName: RouteHandler<HelloWithNameHandler> = async (request, reply) => {
+	const { name = "" } = request.params;
+	reply.status(200).send({ hello: name });
+};
--- src/server/root.ts
+++ src/server/root.ts
@@ -1,5 +1,4 @@
 import { FastifyInstance } from "fastify";
-import { exampleRoute } from "@server/feature/example/route";
 import swagger from "@fastify/swagger";
 import swaggerUI from "@fastify/swagger-ui";
 import { swaggerOptions } from "@server/config/swagger-options";
@@ -7,16 +6,53 @@ import { TypeBoxTypeProvider } from "@fastify/type-provider-typebox";
 import bearerAuthPlugin from "@fastify/bearer-auth";
 import { env } from "@infrastructure/env/server.mjs";
 import helmet from "@fastify/helmet";
+import rateLimit from "@fastify/rate-limit";
+import cors from "@fastify/cors";
+import { examplePublicRoute } from "@server/feature/example-public/route";
+import { examplePrivateRoute } from "@server/feature/example-private/route";
 
-const BEARER_AUTH_KEYS = new Set([env.NEXT_PUBLIC_FASTIFY_API_READ_ONLY_TOKEN, env.FASTIFY_API_WRITE_TOKEN]);
+const BEARER_AUTH_READ_ONLY_KEYS = new Set([env.NEXT_PUBLIC_FASTIFY_API_READ_ONLY_TOKEN]);
+const BEARER_AUTH_WRITE_KEYS = new Set([env.FASTIFY_API_WRITE_TOKEN]);
+
+const ALLOW_ORIGINS = "//localhost:3000, //localhost:1337";
+
 export default async function (server: FastifyInstance) {
+	/* Add type provider */
 	server.withTypeProvider<TypeBoxTypeProvider>();
 
+	/* Register default headers */
 	server.register(helmet, { global: true });
-	server.register(bearerAuthPlugin, { keys: BEARER_AUTH_KEYS });
 
+	await server.register(cors, {
+		origin: ALLOW_ORIGINS,
+		methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+		credentials: true, // Allow cookies to be sent with CORS requests
+	});
+
+	/* Register rate limit */
+	await server.register(rateLimit, {
+		max: 100,
+		timeWindow: "1 minute",
+	});
+
+	/* Register swagger */
 	await server.register(swagger);
 	await server.register(swaggerUI, swaggerOptions);
 
-	server.register(exampleRoute);
+	/* Register public routes */
+	const publicRoutes = async (publicServer: FastifyInstance) => {
+		publicServer.register(bearerAuthPlugin, { keys: BEARER_AUTH_READ_ONLY_KEYS });
+
+		publicServer.register(examplePublicRoute);
+	};
+
+	/* Register private routes */
+	const privateRoutes = async (privateServer: FastifyInstance) => {
+		privateServer.register(bearerAuthPlugin, { keys: BEARER_AUTH_WRITE_KEYS });
+
+		privateServer.register(examplePrivateRoute);
+	};
+
+	server.register(publicRoutes);
+	server.register(privateRoutes);
 }
