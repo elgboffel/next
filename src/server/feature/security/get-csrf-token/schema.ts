0e53f7c2eb6aea1a81dd684afd75240aab0a6879
jok <jok@kruso.dk>
2023-04-27 12:16:07 +0200

    Remove csrf

--- src/server/feature/security/get-csrf-token/schema.ts
+++
@@ -1,5 +0,0 @@
-﻿import { Static, Type } from "@sinclair/typebox";
-
-export const GetCsrfTokenReplySchema = Type.Object({ token: Type.String() });
-
-export type GetCsrfTokenReplyType = Static<typeof GetCsrfTokenReplySchema>;
