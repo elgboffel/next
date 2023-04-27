be5bdd0999b08aebfedcc83c3bf403b6f6de0799
jok <jok@kruso.dk>
2023-04-27 12:16:07 +0200

    Remove csrf

--- src/server/feature/security/handle-csrf-routing.ts
+++
@@ -1,7 +0,0 @@
-﻿import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
-
-export async function handleCsrfRouting(server: FastifyInstance, request: FastifyRequest, reply: FastifyReply) {
-	if (request.raw.url?.startsWith("/api/serverless/security/get-csrf-token")) return;
-
-	return server.csrfProtection(request, reply, () => "");
-}
