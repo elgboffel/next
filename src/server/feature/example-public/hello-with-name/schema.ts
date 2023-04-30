7d5caf6808bf79df62f40411e6b522f53bdf4e26
jok <jok@kruso.dk>
2023-04-30 21:04:06 +0200

    Adds fastify sensible and optimized clinic js scripts

--- src/server/feature/example-public/hello-with-name/schema.ts
+++ src/server/feature/example-public/hello-with-name/schema.ts
@@ -4,7 +4,10 @@ export const HelloWithNameQuerySchema = Type.Object({
 	name: Type.String().Required,
 });
 
-export const HelloWithNameReplySchema = Type.Object({ hello: Type.String() });
+export const HelloWithNameReplySchema = Type.Object({
+	hello: Type.String(),
+	payload: Type.Object({ date: Type.Number(), id: Type.String() }),
+});
 
 export type HelloWithNameQueryType = Static<typeof HelloWithNameQuerySchema>;
 export type HelloWithNameReplyType = Static<typeof HelloWithNameReplySchema>;
